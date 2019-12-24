e534d6aad6f7c4ac3fdb36b316e54002e9c84855
Kamal Maged <kamal_maged@yahoo.com>
2019-12-23 22:38:32 -0500

    Adding title bar

--- CoinPort/AppDelegate.swift
+++ CoinPort/AppDelegate.swift
@@ -16,7 +16,7 @@ import FBSDKLoginKit
 class AppDelegate: UIResponder, UIApplicationDelegate {
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
-        // Override point for customization after application launch.
+        
         FirebaseApp.configure()
         GIDSignIn.sharedInstance().clientID = FirebaseApp.app()?.options.clientID
         GIDSignIn.sharedInstance().delegate = self as? GIDSignInDelegate
