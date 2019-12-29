cad82bc33892b25ec01c7865768308f0b6fae46a
Kamal Maged <kamal_maged@yahoo.com>
2019-12-29 16:58:33 -0500

    Custom navigation bar

--- CoinPort/Controllers/HomeViewController.swift
+++ CoinPort/Controllers/HomeViewController.swift
@@ -16,22 +16,33 @@ class HomeViewController: UIViewController, UINavigationBarDelegate {
         super.viewDidLoad()
         view.backgroundColor = .white
         configureNavigationBar()
+        navigationController?.setNavigationBarHidden(true, animated: false)
 
     }
-    func position(for bar: UIBarPositioning) -> UIBarPosition {
-        return .topAttached
-    }
-    func configureNavigationBar() {
-        
-        navigationController?.navigationBar.barTintColor = UIColor(red: 71/255, green: 91/255, blue: 195/255, alpha: 1)
-        navigationController?.navigationBar.barStyle = .black
-        
-        navigationItem.title = "Global Offers"
-        navigationItem.titleView?.tintColor = .white
     
-        navigationItem.leftBarButtonItem = UIBarButtonItem(image: #imageLiteral(resourceName: "ic_menu_white_3x").withRenderingMode(.alwaysOriginal), style: .plain, target: self, action: #selector(handleMenuToggle))
+    func configureNavigationBar() {
+        let item = UINavigationItem()
+        item.title = "Global Offers"
+        item.titleView?.tintColor = .white
+        item.leftBarButtonItem = UIBarButtonItem(image: #imageLiteral(resourceName: "ic_menu_white_3x").withRenderingMode(.alwaysOriginal), style: .plain, target: self, action: #selector(handleMenuToggle))
+        
+        let navigationBar = UINavigationBar()
+        navigationBar.delegate = self
+        navigationBar.isTranslucent = false
+        navigationBar.barTintColor = UIColor(red: 71/255, green: 91/255, blue: 195/255, alpha: 1)
+        navigationBar.barStyle = .black
+        
+        view.addSubview(navigationBar)
+        navigationBar.translatesAutoresizingMaskIntoConstraints = false
+        navigationBar.leftAnchor.constraint(equalTo: view.leftAnchor).isActive = true
+        navigationBar.rightAnchor.constraint(equalTo: view.rightAnchor).isActive = true
+        navigationBar.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor).isActive = true
+        
+        navigationBar.items = [item]
         
     }
+    
+    
     
     @objc func handleMenuToggle() {
         delegate?.handleMenuToggle()
